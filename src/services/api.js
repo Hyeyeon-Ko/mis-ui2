a3fe4c7e0693076363eb07d938da554660b13d74
Administrator <gitlab_admin_c60c37@example.com>
2024-07-09 22:37:33 +0000

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Axios 인스턴스 생성
 const apiClient = axios.create({
-  baseURL: 'http://101.1.10.87:8080', // API 개발서버
+  baseURL: 'http://kmi-mis-backend-1:8080', // API 개발서버
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json', // 필요에 따라 헤더 설정
@@ -21,7 +21,13 @@ const postData = async () => {
     console.log('POST 요청 성공:', response.data);
     return response.data; // 성공적으로 데이터를 받은 경우 데이터 반환
   } catch (error) {
-    console.error('POST 요청 실패:', error.response ? error.response.data : error.message);
+    if (error.response) {
+      console.error('서버 응답 에러:', error.response.data);
+    } else if (error.request) {
+      console.error('응답 없음:', error.request);
+    } else {
+      console.error('요청 설정 에러:', error.message);
+    }
     throw error; // 에러 발생 시 에러를 throw하여 처리
   }
 };
