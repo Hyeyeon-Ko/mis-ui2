69a6c2174141098fc61e6341d7c9f92f168056e0
Administrator <gitlab_admin_c60c37@example.com>
2024-07-09 07:11:39 +0000

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
-// Axios 기본 설정
-axios.defaults.baseURL = 'http://101.1.10.87:8080'; // api 개발서버
-axios.defaults.timeout = 10000;
+// Axios 인스턴스 생성
+const apiClient = axios.create({
+  baseURL: 'http://101.1.10.87:8080', // API 개발서버
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json', // 필요에 따라 헤더 설정
+  }
+});
 
 // POST 요청을 보내는 함수
 const postData = async () => {
@@ -16,7 +21,7 @@ const postData = async () => {
     console.log('POST 요청 성공:', response.data);
     return response.data; // 성공적으로 데이터를 받은 경우 데이터 반환
   } catch (error) {
-    console.error('POST 요청 실패:', error);
+    console.error('POST 요청 실패:', error.response ? error.response.data : error.message);
     throw error; // 에러 발생 시 에러를 throw하여 처리
   }
 };
