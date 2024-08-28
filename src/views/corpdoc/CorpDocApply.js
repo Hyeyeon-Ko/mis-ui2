e0198da91ed11042c100bdbd3913affe44e9eac3
sungah <sungah0815@gmail.com>
2024-08-28 09:41:46 +0900

    FEAT: 법인서류 신청화면 추가 요청사항

--- src/views/corpdoc/CorpDocApply.js
+++ src/views/corpdoc/CorpDocApply.js
@@ -24,6 +24,12 @@ function CorpDocApply() {
                                 />
                             </div>
                             <div className='corpDoc-form-group'>
+                                <label>사용목적</label>
+                                <textarea
+                                    name="purpose"
+                                />
+                            </div>
+                            <div className='corpDoc-form-group'>
                                 <label>사용일자</label>
                                 <input
                                     type="text"
@@ -39,26 +45,51 @@ function CorpDocApply() {
                             </div>
                             <div className='corpDoc-form-group'>
                                 <label>필요서류/수량</label>
-                                <select name="quantity">
-                                  <option value="document1">선택하세요</option>
-                                  <option value="document1">1</option>
-                                  <option value="document2">2</option>                        
-                                  <option value="document2">3</option>                        
-                                </select>
+                                <div className='corpDoc-form-group-inline'>
+                                    <input type="checkbox" name="document2" value="document2" />
+                                    <label> 법인인감증명서</label>
+                                    <div className='corpDoc-form-group-inline-num'>
+                                        <label> 수량:</label>
+                                        <input type="text" name="quantity2"/>
+                                        <label> 부</label>
+                                    </div>
+                                </div>
+                                <div className='corpDoc-form-group-inline'>
+                                    <input type="checkbox" name="document1" value="document1" />
+                                    <label> 법인등기사항전부증명서(등기부등본)</label>
+                                    <div className='corpDoc-form-group-inline-num'>
+                                        <label> 수량:</label>
+                                        <input type="text" name="quantity1"/>
+                                        <label> 부</label>
+                                    </div>
+                                </div>
+                                <div className='corpDoc-form-group-inline'>
+                                    <input type="checkbox" name="document3" value="document3" />
+                                    <label> 사용인감계</label>
+                                    <div className='corpDoc-form-group-inline-num'>
+                                        <label> 수량:</label>
+                                        <input type="text" name="quantity3"/>
+                                        <label> 부</label>
+                                    </div>
+                                </div>
+                                <div className='corpDoc-form-group-inline'>
+                                    <input type="checkbox" name="document4" value="document4" />
+                                    <label> 위임장</label>
+                                    <div className='corpDoc-form-group-inline-num'>
+                                        <label> 수량:</label>
+                                        <input type="text" name="quantity4"/>
+                                        <label> 부</label>
+                                    </div>
+                                </div>
                             </div>
                             <div className='corpDoc-form-group'>
-                                <label>원본/PDF</label>
+                                <label>원본 / pdf</label>
                                 <select name="division">
                                     <option value="option">선택하세요</option>
                                     <option value="original">원본</option>
-                                    <option value="pdf">PDF</option>                        
+                                    <option value="pdf">pdf</option>
+                                    <option value="both">원본 + pdf</option>       value 고치기    
                                 </select>
-                            </div>
-                            <div className='corpDoc-form-group'>
-                                <label>사용목적</label>
-                                <textarea
-                                    name="purpose"
-                                />
                             </div>
                             <div className='corpDoc-form-group'>
                                 <label>특이사항</label>
