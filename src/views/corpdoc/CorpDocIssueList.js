12d51de0185c836d9d37a93d0817a84a568aaecc
sungah <sungah0815@gmail.com>
2024-08-30 16:06:10 +0900

    FEAT: 서류발급대장 화면 개발 및 연동

--- src/views/corpdoc/CorpDocIssueList.js
+++ src/views/corpdoc/CorpDocIssueList.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import Breadcrumb from '../../components/common/Breadcrumb';
+import Button from '../../components/common/Button';
 import ConditionFilter from '../../components/common/ConditionFilter';
 import CorpDocApprovalModal from '../../views/corpdoc/CorpDocApprovalModal';
 import SignitureImage from '../../assets/images/signiture.png';
+import axios from 'axios';
 import '../../styles/corpdoc/CorpDocIssueList.css';
 
 function CorpDocIssueList() {
   const [applications, setApplications] = useState([]);
   const [filteredApplications, setFilteredApplications] = useState([]);
+  const [pendingApplications, setPendingApplications] = useState([]);
+  const [filteredPendingApplications, setFilteredPendingApplications] = useState([]);
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
@@ -15,59 +19,68 @@ function CorpDocIssueList() {
   const [clickedRows, setClickedRows] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const fetchedData = [
-        {
-          id: 1,
-          date: '2024-05-02',
-          submitter: '제출처',
-          usagePurpose: '서류발급',
-          certificate: { incoming: 3, used: 0, left: 18 },
-          registry: { incoming: 0, used: 1, left: 20 },
-          status: '결재완료',
-          applicantName: '홍길동',
-          signitureImage: SignitureImage,
-          approvers: [
-            { name: '김철수', approvalDate: '2024-08-08', signitureImage: SignitureImage },
-            { name: '박영희', approvalDate: '2024-08-10', signitureImage: SignitureImage },
-          ],
-        },
-        {
-          id: 2,
-          date: '2024-05-03',
-          submitter: '제출처2',
-          usagePurpose: '아아아',
-          certificate: { incoming: 5, used: 0, left: 20 },
-          registry: { incoming: 0, used: 9, left: 3 },
-          status: '결재진행중',
-          applicantName: '이영희',
-          approvers: [
-            {},
-          ],
-        },
-      ];
-      setApplications(fetchedData);
-      setFilteredApplications(fetchedData);
-
-      const clickedRows = JSON.parse(localStorage.getItem('clickedRows')) || [];
-      setClickedRows(clickedRows);
-    };
-    fetchData();
+    fetchIssueData();
   }, []);
-  
 
-  const handleSearch = ({ searchType, keyword, startDate, endDate }) => {
-    let filtered = applications;
+  const fetchIssueData = async () => {
+    try {
+      const response = await axios.get('/api/corpDoc/IssueList');
+      console.log("response: ", response);
+
+      if (response.data) {
+        const issueListData = response.data.data.issueList.map(item => ({
+          id: item.draftId,
+          issueDate: item.issueDate,
+          submission: item.submission,
+          purpose: item.purpose,
+          certificate: { incoming: 0, used: item.certCorpseal, left: item.totalCorpseal },
+          registry: { incoming: 0, used: item.certCoregister, left: item.totalCoregister },
+          applicantName: item.drafter,
+          center: item.instNm,
+          approveStatus: '결재진행중',
+          approvers: [],
+          signitureImage: SignitureImage,
+        }));
+
+        const issuePendingListData = response.data.data.issuePendingList.map(item => ({
+          id: item.draftId,
+          date: item.draftDate,
+          useDate: item.useDate,
+          submission: item.submission,
+          purpose: item.purpose,
+          certificate: { incoming: 0, used: item.certCorpseal, left: item.totalCorpseal },
+          registry: { incoming: 0, used: item.certCoregister, left: item.totalCoregister },
+          status: item.status,
+          applicantName: item.drafter,
+          center: item.instNm,
+          approvers: [],
+          signitureImage: SignitureImage,
+        }));
+
+        setApplications(issueListData);
+        setFilteredApplications(issueListData);
+        setPendingApplications(issuePendingListData);
+        setFilteredPendingApplications(issuePendingListData);
+        console.log("applications: ", filteredApplications);
+        console.log("pendingApplications: ", filteredPendingApplications);
+      }
+    } catch (error) {
+      console.error("Error fetching issue data:", error);
+    }
+  };
+
+  const handleSearch = ({ searchType, keyword, startDate, endDate }, listType = 'applications') => {
+    let filtered = listType === 'applications' ? applications : pendingApplications;
 
     if (keyword) {
       filtered = filtered.filter(app => {
-        if (searchType === '제출처') return app.submitter.includes(keyword);
-        if (searchType === '사용목적') return app.usagePurpose.includes(keyword);
+        if (searchType === '제출처') return app.submission.includes(keyword);
+        if (searchType === '사용목적') return app.purpose.includes(keyword);
         if (searchType === '인장구분') return app.status.includes(keyword);
         if (searchType === '전체') {
           return (
-            app.submitter.includes(keyword) ||
-            app.usagePurpose.includes(keyword) ||
+            app.submission.includes(keyword) ||
+            app.purpose.includes(keyword) ||
             app.status.includes(keyword)
           );
         }
@@ -84,7 +97,11 @@ function CorpDocIssueList() {
       });
     }
 
-    setFilteredApplications(filtered);
+    if (listType === 'applications') {
+      setFilteredApplications(filtered);
+    } else {
+      setFilteredPendingApplications(filtered);
+    }
   };
 
   const closeModal = () => {
@@ -119,24 +136,28 @@ function CorpDocIssueList() {
           setStartDate={setStartDate}
           endDate={endDate}
           setEndDate={setEndDate}
-          onSearch={handleSearch}
+          onSearch={(searchParams) => handleSearch(searchParams, 'applications')}
           onReset={() => setFilteredApplications(applications)}
           showDocumentType={false}
           showSearchCondition={true}
           excludeRecipient={true}
         />
         {filteredApplications.length > 0 ? (
-          <table className="table">
+          <table className="corpDoc-issue-table">
             <thead>
               <tr>
-                <th rowSpan="2">일자</th>
+                <th rowSpan="2">No.</th>
+                <th rowSpan="2">발급일자</th>
+                <th colSpan="2">신청자</th>
                 <th rowSpan="2">제출처</th>
                 <th rowSpan="2">사용목적</th>
                 <th colSpan="3">법인인감증명서</th>
-                <th colSpan="3">법인등기부등본</th>
+                <th colSpan="3">법인등기사항전부증명서</th>
                 <th rowSpan="2">결재</th>
               </tr>
               <tr>
+                <th>센터</th>
+                <th>이름</th>
                 <th>입고</th>
                 <th>사용</th>
                 <th>잔고</th>
@@ -148,23 +169,19 @@ function CorpDocIssueList() {
             <tbody>
               {filteredApplications.map((app, index) => (
                 <tr key={index}>
-                  <td>{app.date}</td>
-                  <td>{app.submitter}</td>
-                  <td>{app.usagePurpose}</td>
+                  <td>{index + 1}</td>
+                  <td>{app.issueDate}</td>
+                  <td>{app.center}</td>
+                  <td>{app.applicantName}</td>
+                  <td>{app.submission}</td>
+                  <td>{app.purpose}</td>
                   <td>{app.certificate.incoming}</td>
                   <td>{app.certificate.used}</td>
                   <td>{app.certificate.left}</td>
                   <td>{app.registry.incoming}</td>
                   <td>{app.registry.used}</td>
                   <td>{app.registry.left}</td>
-                  <td
-                    className={`status-${app.status.replace(/\s+/g, '-').toLowerCase()} clickable ${
-                      clickedRows.includes(app.id) ? 'confirmed' : ''
-                    }`}
-                    onClick={() => handleRowClick(app.status, app)}
-                  >
-                    {app.status}
-                  </td>
+                  <td>{app.approveStatus}</td>
                 </tr>
               ))}
             </tbody>
@@ -172,6 +189,51 @@ function CorpDocIssueList() {
         ) : (
           <div>No data available</div>
         )}
+        <div className='corpDoc-issue-pending-list'>
+          <h3>서류 발급 대기 목록</h3>
+          {filteredPendingApplications.length > 0 ? (
+            <table className="table">
+              <thead>
+                <tr>
+                  <th rowSpan="2">No.</th>
+                  <th rowSpan="2">사용일자</th>
+                  <th colSpan="2">신청자</th>
+                  <th rowSpan="2">제출처</th>
+                  <th rowSpan="2">사용목적</th>
+                  <th colSpan="2">필요 수량</th>
+                  <th rowSpan="2">발급</th>
+                </tr>
+                <tr>
+                  <th>센터</th>
+                  <th>이름</th>
+                  <th>법인인감증명서</th>
+                  <th>법인등기사항전부증명서</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredPendingApplications.map((app, index) => (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{app.useDate}</td>
+                    <td>{app.center}</td>
+                    <td>{app.applicantName}</td>
+                    <td>{app.submission}</td>
+                    <td>{app.purpose}</td>
+                    <td>{app.certificate.used}</td>
+                    <td>{app.registry.used}</td>
+                    <td>
+                    <Button className="confirm-issue-button">
+                      발 급
+                    </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <div>No pending data available</div>
+          )}
+        </div>
       </div>
       {modalVisible && selectedDocumentDetails && (
         <CorpDocApprovalModal
