9a8a53251075de93757eab213b72a2e903ad3ccb
clsung <clsung@kmi.or.kr>
2024-09-04 11:07:00 +0900

    build error modify

--- src/components/common/ConditionFilter.js
+++ src/components/common/ConditionFilter.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import Button from '../common/Button';
 import '../../styles/common/ConditionFilter.css';
