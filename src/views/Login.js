24f771eb175c7555cfefe814b609ccd013b1eb95
clsung <clsung@kmi.or.kr>
2024-09-25 10:59:36 +0900

    merge

--- src/views/Login.js
+++ src/views/Login.js
@@ -39,7 +39,7 @@ const Login = () => {
         const data = await response.json();
 
         if (data && data.data) {
-          const authorityResponse = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/standardData`, {
+          const authorityResponse = await fetch(`/api/auth/standardData`, {
             method: 'GET',
             headers: {
               'Content-Type': 'application/json',
