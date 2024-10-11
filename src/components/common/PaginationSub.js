fdc702c117f1897145e824fcb4639df07126f488
gaia <gaia@kmi.or.kr>
2024-10-11 09:32:39 +0900

    Fix: 페이지네이션 프롭 수정

--- src/components/common/PaginationSub.js
+++ src/components/common/PaginationSub.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactPaginate from 'react-paginate';
-import '../../styles/common/Pagination.css';
+import React from "react";
+import ReactPaginate from "react-paginate";
+import "../../styles/common/Pagination.css";
 
 function PaginationSub({ totalPages, onPageChange, currentPage }) {
   return (
-    <div className='pagination-layout'>
-    <ReactPaginate
-        breakLabel={''}
+    <div className="pagination-layout">
+      <ReactPaginate
+        breakLabel={""}
         nextLabel=">"
         onPageChange={onPageChange}
-        pageRangeDisplayed={5} 
-        pageCount={totalPages} 
+        pageRangeDisplayed={Math.ceil(totalPages)}
+        pageCount={Math.ceil(totalPages)}
         previousLabel="<"
         renderOnZeroPageCount={null}
         containerClassName="pagination"
         activeClassName="active"
-        forcePage={currentPage - 1} 
-    />
+        forcePage={currentPage - 1}
+      />
     </div>
   );
 }
