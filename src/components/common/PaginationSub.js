5f6fb090e1c58574bb903a1077a498c8b6c36f8c
Hyeyeon <khy33355@gmail.com>
2024-10-14 09:23:46 +0900

    FIX: PaginationSub 파일 분리

--- src/components/common/PaginationSub.js
+++ src/components/common/PaginationSub.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../styles/common/Pagination.css";
+import "../../styles/common/PaginationSub.css";
 
 function PaginationSub({ totalPages, onPageChange, currentPage }) {
   const pageGroupSize = 5;
@@ -11,11 +11,11 @@ function PaginationSub({ totalPages, onPageChange, currentPage }) {
   const pageNumbers = Array.from({ length: endPage - startPage + 1 }, (_, index) => startPage + index);
 
   return (
-    <div className="pagination-layout">
+    <div className="pagination-sub-layout">
       <button
         onClick={() => onPageChange({ selected: Math.max(0, startPage - 2) })} 
         disabled={startPage === 1}
-        className={`pagination-button ${startPage === 1 ? 'disabled' : ''}`}
+        className={`pagination-sub-button ${startPage === 1 ? 'disabled' : ''}`}
       >
         &lt;
       </button>
@@ -23,7 +23,7 @@ function PaginationSub({ totalPages, onPageChange, currentPage }) {
         <button
           key={page}
           onClick={() => onPageChange({ selected: page - 1 })} 
-          className={`pagination-button ${currentPage === page ? 'active' : ''}`}
+          className={`pagination-sub-button ${currentPage === page ? 'active' : ''}`}
         >
           {page}
         </button>
@@ -31,7 +31,7 @@ function PaginationSub({ totalPages, onPageChange, currentPage }) {
       <button
         onClick={() => onPageChange({ selected: Math.min(totalPages - 1, endPage) })} 
         disabled={endPage === totalPages}
-        className={`pagination-button ${endPage === totalPages ? 'disabled' : ''}`}
+        className={`pagination-sub-button ${endPage === totalPages ? 'disabled' : ''}`}
       >
         &gt;
       </button>
